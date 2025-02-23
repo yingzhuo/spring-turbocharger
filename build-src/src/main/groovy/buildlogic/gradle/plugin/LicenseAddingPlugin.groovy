924f954477dbf0e32d77f2c53a46f3f47d43d533
应卓 <yingzhor@gmail.com>
2025-02-23 18:15:52 +0800

    2025-02-23 18:15:52

--- build-src/src/main/groovy/buildlogic/gradle/plugin/LicenseAddingPlugin.groovy
+++ build-src/src/main/groovy/buildlogic/gradle/plugin/LicenseAddingPlugin.groovy
@@ -18,49 +18,60 @@
 package buildlogic.gradle.plugin
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.StopExecutionException
+import org.gradle.api.tasks.TaskAction
 
-class LicenseAddingPlugin extends AbstractPlugin {
+import javax.inject.Inject
 
-	final static String TASK_NAME_ADD_JAVA_LICENSE_HEADER = "addLicenseHeader"
+class LicenseAddingPlugin implements Plugin<Project> {
+
+	final static String TASK_NAME_ADD_LICENSE_HEADER = "addLicenseHeader"
 
 	@Override
 	void apply(Project project) {
-		super.setExtensionsBean(project, TASK_NAME_ADD_JAVA_LICENSE_HEADER, new AddJavaLicenseHeaderExt())
-
-		project
-			.getTasks()
-			.register(TASK_NAME_ADD_JAVA_LICENSE_HEADER, DefaultTask) { task ->
-				task.group = 'license'
-				task.description = 'Adds license header for the Java source files'
-
-				var config = getExtensionsBean(project, TASK_NAME_ADD_JAVA_LICENSE_HEADER) as AddJavaLicenseHeaderExt
-				var header = config.javaHeader
-				if (!header.endsWith('\n')) {
-					header += '\n'
-				}
-
-				if (header.isBlank()) {
-					throw new StopExecutionException('java license header text is blank')
-				}
-
-				doLast {
-					project.fileTree(project.rootDir) {
-						include '**/*.java'
-					}.each { file ->
-						def content = file.text
-						if (!content.startsWith(header)) {
-							file.setText(
-								header + content
-							)
-						}
-					}
-				}
-			}
+		project.extensions.add(TASK_NAME_ADD_LICENSE_HEADER, new Ext())
+		project.getTasks()
+			.register(TASK_NAME_ADD_LICENSE_HEADER, AddLicenseHeaderTask, project)
 	}
 
-	static class AddJavaLicenseHeaderExt {
+	static class AddLicenseHeaderTask extends DefaultTask {
+		final Project project;
+
+		@Inject
+		AddLicenseHeaderTask(Project project) {
+			this.project = project
+			this.group = 'license'
+			this.description = 'Adds license header for the Java source files'
+		}
+
+		@TaskAction
+		void execute() {
+			var ext = project.extensions.getByName(TASK_NAME_ADD_LICENSE_HEADER) as Ext
+			var header = ext.javaHeader
+			if (!header.endsWith('\n')) {
+				header += '\n'
+			}
+
+			if (header.isBlank()) {
+				throw new StopExecutionException('java license header text is blank')
+			}
+
+			project.fileTree(project.rootDir) {
+				include '**/*.java'
+			}.each { file ->
+				def content = file.text
+				if (!content.startsWith(header)) {
+					file.setText(
+						header + content
+					)
+				}
+			}
+		}
+	}
+
+	static class Ext {
 		String javaHeader = ''
 	}
 
