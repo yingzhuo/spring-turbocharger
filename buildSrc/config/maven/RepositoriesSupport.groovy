af5858f9255b5d2aadcad3dc443951998759f630
应卓 <yingzhor@gmail.com>
2025-05-02 10:17:48 +0800

    2025-05-02 10:17:48

---
+++ buildSrc/config/maven/RepositoriesSupport.groovy
@@ -0,0 +1,111 @@
+/*
+ *
+ * Copyright 2022-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+def applyPluginManagement(settings) {
+
+	settings.pluginManagement.repositories {
+		mavenLocal()
+		maven {
+			name = '阿里云'
+			url = 'https://maven.aliyun.com/repository/public/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		maven {
+			name = '腾讯云'
+			url = 'https://mirrors.cloud.tencent.com/nexus/repository/maven-public/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		maven {
+			name = '华为云'
+			url = 'https://repo.huaweicloud.com/repository/maven/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		maven {
+			name = '网易云'
+			url = 'https://mirrors.163.com/maven/repository/maven-public/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		mavenCentral()
+		gradlePluginPortal()
+		maven {
+			url = 'https://repo.spring.io/release'
+		}
+		maven {
+			url = 'https://repo.spring.io/milestone'
+		}
+		maven {
+			url = 'https://repo.spring.io/snapshot'
+		}
+		google()
+	}
+}
+
+def applyDependencyResolutionManagement(settings) {
+	settings.dependencyResolutionManagement.repositories {
+		mavenLocal()
+		maven {
+			name = '阿里云'
+			url = 'https://maven.aliyun.com/repository/public/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		maven {
+			name = '腾讯云'
+			url = 'https://mirrors.cloud.tencent.com/nexus/repository/maven-public/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		maven {
+			name = '华为云'
+			url = 'https://repo.huaweicloud.com/repository/maven/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		maven {
+			name = '网易云'
+			url = 'https://mirrors.163.com/maven/repository/maven-public/'
+			content {
+				excludeGroup 'com.github.yingzhuo'
+			}
+		}
+		mavenCentral()
+		gradlePluginPortal()
+		maven {
+			url = 'https://repo.spring.io/release'
+		}
+		maven {
+			url = 'https://repo.spring.io/milestone'
+		}
+		maven {
+			url = 'https://repo.spring.io/snapshot'
+		}
+		google()
+	}
+}
+
+return this
