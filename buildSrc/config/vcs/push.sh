72f43e35e6bb77f63c585c9b5dac6eb367fb480a
应卓 <yingzhor@gmail.com>
2025-05-09 10:33:18 +0800

    2025-05-09 10:33:18

--- buildSrc/config/vcs/push.sh
+++ buildSrc/config/vcs/push.sh
@@ -3,7 +3,8 @@ set -e
 
 # 检查是否有未提交的更改
 if git diff --quiet && git diff --staged --quiet; then
-    echo "[INFO] 没有检测到更改，跳过提交。"
+    echo "[INFO] 没有检测到更改"
+    echo "[INFO] 跳过提交"
     exit 0
 fi
 
@@ -12,6 +13,7 @@ git add .
 TIMESTAMP=$(date "+%F %T")
 git commit -m "$TIMESTAMP"
 
+# 推送到所有远程仓库
 REMOTES=$(git remote)
 for remote in $REMOTES; do
     echo "[INFO] 正在推送至远程仓库：$remote"
