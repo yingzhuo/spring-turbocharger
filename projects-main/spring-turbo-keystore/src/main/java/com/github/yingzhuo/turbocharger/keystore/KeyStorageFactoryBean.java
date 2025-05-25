867f383ab93467c5f2725181ea66bf635cff0dff
应卓 <yingzhor@gmail.com>
2025-05-26 05:43:03 +0800

    2025-05-26 05:43:03

---
+++ projects-main/spring-turbo-keystore/src/main/java/com/github/yingzhuo/turbocharger/keystore/KeyStorageFactoryBean.java
@@ -0,0 +1,51 @@
+/*
+ *
+ * Copyright 2022-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.github.yingzhuo.turbocharger.keystore;
+
+import lombok.Setter;
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * @author 应卓
+ * @since 3.5.0
+ */
+public class KeyStorageFactoryBean implements FactoryBean<KeyStorage> {
+
+	@Setter
+	private String location;
+
+	@Setter
+	private String storepass;
+
+	@Setter
+	private KeyStoreFormat keyStoreFormat = KeyStoreFormat.PKCS12;
+
+	@Override
+	public KeyStorage getObject() throws Exception {
+		return switch (keyStoreFormat) {
+			case PKCS12 -> KeyStorage.loadFromPkcs12(location, storepass);
+			case JKS -> KeyStorage.loadFromJks(location, storepass);
+		};
+	}
+
+	@Override
+	public final Class<?> getObjectType() {
+		return KeyStorage.class;
+	}
+
+}
