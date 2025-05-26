678faf215b0af9946e06c42d7cf3ff87a3cf651e
应卓 <yingzhor@gmail.com>
2025-05-26 11:37:59 +0800

    2025-05-26 11:37:59

--- projects-example/spring-security-jwt/src/main/java/examples/security/JwtTokenToUserConverter.java
+++ projects-example/spring-security-jwt/src/main/java/examples/security/JwtTokenToUserConverter.java
@@ -23,12 +23,14 @@ import com.github.yingzhuo.turbocharger.security.authentication.TokenToUserConve
 import com.github.yingzhuo.turbocharger.security.jwt.AbstractJwtTokenToUserConverter;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.lang.Nullable;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
 import java.util.stream.Stream;
 
 @Component
@@ -41,34 +43,43 @@ public class JwtTokenToUserConverter extends AbstractJwtTokenToUserConverter imp
 	@Nullable
 	@Override
 	protected UserDetails doAuthenticate(String rawToken, String headerJson, String payloadJson) throws AuthenticationException {
-		var ua = JsonUtils.parseJson(payloadJson, UserAttributes.class);
-
-		var rolesToUse = Stream.of(ua.roles)
-			.map(role -> {
-				if (role.startsWith("ROLE_")) {
-					return role.substring("ROLE_".length());
-				} else {
-					return role;
-				}
-			})
-			.toList()
-			.toArray(new String[0]);
-
-		return User.builder()
-			.username(ua.getUsername())
-			.password("WhatEver")
-			.roles(rolesToUse)
-			.build();
+		return JsonUtils.parseJson(payloadJson, UserAttributes.class)
+			.toUserDetails();
 	}
 
 	// -----------------------------------------------------------------------------------------------------------------
 
 	@Getter
 	@Setter
-	public static class UserAttributes {
+	@ToString
+	private static class UserAttributes implements Serializable {
 		private String userId;
 		private String username;
 		private String[] roles;
+
+		private UserDetails toUserDetails() {
+			return User.builder()
+				.username(username)
+				.password("<DO NOT NEED PASSWORD>")
+				.roles(prettyRoles())
+				.disabled(false)
+				.accountExpired(false)
+				.credentialsExpired(false)
+				.build();
+		}
+
+		private String[] prettyRoles() {
+			return Stream.of(roles)
+				.map(role -> {
+					if (role.startsWith("ROLE_")) {
+						return role.substring("ROLE_".length()).trim();
+					} else {
+						return role.trim();
+					}
+				})
+				.toList()
+				.toArray(new String[0]);
+		}
 	}
 
 }
