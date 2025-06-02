0fa75be3c184165d5f2a8dc23c929ec1a8bc5472
应卓 <yingzhor@gmail.com>
2025-06-02 19:56:49 +0800

    2025-06-02 19:56:49

---
+++ projects-main/spring-turbo-security/src/main/java/com/github/yingzhuo/turbocharger/security/encryptor/RsaSecretEncryptorFactoryBean.java
@@ -0,0 +1,52 @@
+/*
+ *
+ * Copyright 2022-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.github.yingzhuo.turbocharger.security.encryptor;
+
+import lombok.Setter;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.security.crypto.encrypt.RsaAlgorithm;
+import org.springframework.security.crypto.encrypt.RsaSecretEncryptor;
+
+import java.security.KeyPair;
+
+/**
+ * @author 应卓
+ * @since 3.5.0
+ */
+public class RsaSecretEncryptorFactoryBean implements FactoryBean<RsaSecretEncryptor> {
+
+	@Setter
+	private RsaAlgorithm rsaAlgorithm;
+
+	@Setter
+	private KeyPair keyPair;
+
+	@Setter
+	private String salt = "deadbeef";
+
+	@Override
+	public RsaSecretEncryptor getObject() {
+		return new RsaSecretEncryptor(keyPair, rsaAlgorithm, salt);
+	}
+
+	@Override
+	public final Class<?> getObjectType() {
+		return RsaSecretEncryptor.class;
+	}
+
+}
