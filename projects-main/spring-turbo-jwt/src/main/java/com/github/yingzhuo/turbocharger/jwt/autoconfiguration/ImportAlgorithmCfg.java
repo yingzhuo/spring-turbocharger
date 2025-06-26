2c8bc0f0aeaa728cd3f5d4878610fbdf4baef490
应卓 <yingzhor@gmail.com>
2025-06-26 15:27:26 +0800

    2025-06-26 15:27:26

--- projects-main/spring-turbo-jwt/src/main/java/com/github/yingzhuo/turbocharger/jwt/autoconfiguration/ImportAlgorithmCfg.java
+++ projects-main/spring-turbo-jwt/src/main/java/com/github/yingzhuo/turbocharger/jwt/autoconfiguration/ImportAlgorithmCfg.java
@@ -19,19 +19,19 @@ package com.github.yingzhuo.turbocharger.jwt.autoconfiguration;
 
 import com.auth0.jwt.algorithms.Algorithm;
 import com.github.yingzhuo.turbocharger.bean.ImportBeanDefinitionRegistrarSupport;
+import com.github.yingzhuo.turbocharger.util.StringPool;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.boot.ssl.pem.PemContent;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.util.StringUtils;
 
-import java.nio.charset.StandardCharsets;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.stream.Collectors;
 
 /**
  * @author 应卓
@@ -53,15 +53,31 @@ class ImportAlgorithmCfg extends ImportBeanDefinitionRegistrarSupport {
 			var primary = attr.getBoolean("primary");
 			var scope = attr.getString("scope");
 
+			var pemText = super.getResourceAsLines(location)
+				.map(String::trim)
+				.filter(StringUtils::hasText)
+				.collect(Collectors.joining(StringPool.LF));
+
+			var pc = PemContent.of(pemText);
+			var publicKey = pc.getCertificates().get(0).getPublicKey();
+			var privateKey = pc.getPrivateKey(keypass);
+
+			var alg = switch (kind) {
+				case RSA256 -> Algorithm.RSA256((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
+				case RSA384 -> Algorithm.RSA384((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
+				case RSA512 -> Algorithm.RSA512((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
+				case ECDSA256 -> Algorithm.ECDSA256((ECPublicKey) publicKey, (ECPrivateKey) privateKey);
+				case ECDSA384 -> Algorithm.ECDSA384((ECPublicKey) publicKey, (ECPrivateKey) privateKey);
+				case ECDSA512 -> Algorithm.ECDSA512((ECPublicKey) publicKey, (ECPrivateKey) privateKey);
+			};
+
 			var beanDef =
-				BeanDefinitionBuilder.genericBeanDefinition(Algorithm.class)
+				BeanDefinitionBuilder.genericBeanDefinition(Algorithm.class, () -> alg)
 					.setScope(scope)
 					.setAbstract(false)
 					.setLazyInit(false)
 					.setPrimary(primary)
 					.getBeanDefinition();
-
-			beanDef.setInstanceSupplier(new AlgorithmSupplier(getResourceLoader(), location, keypass, kind));
 
 			if (!StringUtils.hasText(beanName)) {
 				beanName = beanNameGen.generateBeanName(beanDef, registry);
@@ -72,36 +88,6 @@ class ImportAlgorithmCfg extends ImportBeanDefinitionRegistrarSupport {
 			for (var alias : attr.getStringArray("aliasesOfBean")) {
 				registry.registerAlias(beanName, alias);
 			}
-		}
-	}
-
-	private static class AlgorithmSupplier extends BeanInstanceSupplier<Algorithm> {
-
-		private final ResourceLoader resourceLoader;
-		private final String location;
-		private final String keypass;
-		private final AlgorithmKind kind;
-
-		public AlgorithmSupplier(ResourceLoader resourceLoader, String location, String keypass, AlgorithmKind kind) {
-			this.resourceLoader = resourceLoader;
-			this.location = location;
-			this.keypass = keypass;
-			this.kind = kind;
-		}
-
-		protected Algorithm doGet() throws Exception {
-			var pc = PemContent.of(resourceLoader.getResource(location).getContentAsString(StandardCharsets.UTF_8));
-			var publicKey = pc.getCertificates().get(0).getPublicKey();
-			var privateKey = pc.getPrivateKey(keypass);
-
-			return switch (kind) {
-				case RSA256 -> Algorithm.RSA256((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
-				case RSA384 -> Algorithm.RSA384((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
-				case RSA512 -> Algorithm.RSA512((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
-				case ECDSA256 -> Algorithm.ECDSA256((ECPublicKey) publicKey, (ECPrivateKey) privateKey);
-				case ECDSA384 -> Algorithm.ECDSA384((ECPublicKey) publicKey, (ECPrivateKey) privateKey);
-				case ECDSA512 -> Algorithm.ECDSA512((ECPublicKey) publicKey, (ECPrivateKey) privateKey);
-			};
 		}
 	}
 
