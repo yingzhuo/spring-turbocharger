8eaf251dd1c932dbbbb96dc17f399e07dca97ada
应卓 <yingzhor@gmail.com>
2025-07-02 13:50:13 +0800

    2025-07-02 13:50:13

--- projects-main/spring-turbo-jwt/src/main/java/com/github/yingzhuo/turbocharger/jwt/algorithm/GenericAlgorithm.java
+++ projects-main/spring-turbo-jwt/src/main/java/com/github/yingzhuo/turbocharger/jwt/algorithm/GenericAlgorithm.java
@@ -25,6 +25,7 @@ import org.springframework.lang.Nullable;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Map;
 
 /**
  * 通用型JWT算法
@@ -38,6 +39,7 @@ public class GenericAlgorithm extends AbstractAlgorithm {
 	private final String sigAlgName;
 	private final PublicKey publicKey;
 	private final PrivateKey privateKey;
+	private final Map<String, String> algNameMapping;
 
 	/**
 	 * 构造方法
@@ -56,6 +58,16 @@ public class GenericAlgorithm extends AbstractAlgorithm {
 	 */
 	public GenericAlgorithm(String pemContent, @Nullable String password) {
 		super("<no name>");
+		this.algNameMapping =
+			Map.of(
+				"SHA256withRSA", "RS256",
+				"SHA385withRSA", "RS384",
+				"SHA512withRSA", "RS512",
+				"SHA256withECDSA", "ES256",
+				"SHA384withECDSA", "ES384",
+				"SHA512withECDSA", "ES512"
+			);
+
 		var pc = PemContent.of(pemContent);
 		var cert = pc.getCertificates().get(0);
 		this.sigAlgName = cert.getSigAlgName();
@@ -68,7 +80,8 @@ public class GenericAlgorithm extends AbstractAlgorithm {
 	 */
 	@Override
 	public String getName() {
-		return this.sigAlgName;
+		var newName = algNameMapping.get(sigAlgName);
+		return newName != null ? newName : sigAlgName;
 	}
 
 	/**
