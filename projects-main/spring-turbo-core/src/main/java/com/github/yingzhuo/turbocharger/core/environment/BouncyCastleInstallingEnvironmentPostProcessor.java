083f3ebb6b0f734442cdee177e7acb594c2dc3bb
应卓 <yingzhor@gmail.com>
2026-03-03 09:13:12 +0800

    2026-03-03 09:13:12

--- projects-main/spring-turbo-core/src/main/java/com/github/yingzhuo/turbocharger/core/environment/BouncyCastleInstallingEnvironmentPostProcessor.java
+++ projects-main/spring-turbo-core/src/main/java/com/github/yingzhuo/turbocharger/core/environment/BouncyCastleInstallingEnvironmentPostProcessor.java
@@ -15,8 +15,10 @@
  */
 package com.github.yingzhuo.turbocharger.core.environment;
 
+import org.apache.commons.logging.Log;
 import org.springframework.boot.EnvironmentPostProcessor;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.logging.DeferredLogFactory;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.security.Provider;
@@ -36,6 +38,12 @@ public class BouncyCastleInstallingEnvironmentPostProcessor implements Environme
 
 	private static final String BOUNCY_CASTLE_PROVIDER_CLASS = "org.bouncycastle.jce.provider.BouncyCastleProvider";
 
+	private final Log log;
+
+	public BouncyCastleInstallingEnvironmentPostProcessor(DeferredLogFactory logFactory) {
+		this.log = logFactory.getLog(BouncyCastleInstallingEnvironmentPostProcessor.class);
+	}
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -46,6 +54,7 @@ public class BouncyCastleInstallingEnvironmentPostProcessor implements Environme
 			var ctor = cls.getConstructor();
 			var provider = ctor.newInstance();
 			Security.addProvider((Provider) provider);
+			log.debug("BouncyCastle provider class: " + provider);
 		} catch (Throwable ignored) {
 			// noop
 		}
